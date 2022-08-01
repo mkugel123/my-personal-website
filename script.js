0f8ce5b984443c765af7efa41613606313252d68
Zalman <moishkugel@gmail.com>
2022-08-01 15:26:20 -0400

    add javascript, finish website

---
+++ script.js
@@ -0,0 +1,17 @@
+const start = document.getElementById("zero");
+const baby = document.getElementById("baby");
+start.addEventListener('click', function() {
+    baby.style.display = "block";
+})
+
+const middle = document.getElementById("fifty");
+const confused = document.getElementById("confused");
+middle.addEventListener('click', function() {
+    confused.style.display = "block";
+})
+
+const end = document.getElementById("hundred");
+const einstein = document.getElementById("einstein");
+end.addEventListener('click', function() {
+    einstein.style.display = "block";
+})
